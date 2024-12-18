264d5d404345201e3c5accde76057aba785ba9a4
architdhiman <78320064+architdhiman@users.noreply.github.com>
2024-12-18 11:54:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var number = document.getElementById(".number")
+console.log(number);
+
+var start = document.querySelector(".start")
+start.addEventListener("click", () =>{
+    start.innerHTML ="checking"
+})
+
+var playarea = document.getElementById(".playarea")
+var clickcount=0;
+playarea.addEventListener("click", () =>{
+    clickcount++;
+    list.appendChild(count)
+
+})
+
+var interval = document.getElementById(".interval")
+interval.addEventListener("click", () =>{
+    interval.innerHTML="clickedd"
+    interval.conte
+})
+
+var list = document.getElementById(".list")
